0f7eb560898fb856b3486ddc311f801533f86e74
MAHALAKSHMI150506 <mahalakshmikan1505@gmail.com>
2026-01-27 15:06:28 +0530

    made changes for authentication

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -5,7 +5,7 @@ import './index.css';
 import { GoogleOAuthProvider } from '@react-oauth/google';
 import { AuthProvider } from './context/AuthContext.jsx';
 
-const GOOGLE_CLIENT_ID = "918757837578-fhos6lipkqofmsbemio9k0i7a3mi8uio.apps.googleusercontent.com";
+const GOOGLE_CLIENT_ID = import.meta.env.VITE_GOOGLE_CLIENT_ID || "918757837578-fhos6lipkqofmsbemio9k0i7a3mi8uio.apps.googleusercontent.com";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
